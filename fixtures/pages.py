37939d4dbbab6da088b3db00f406ef7332fb6a60
Nikita Filonov <nikita.filonov@dif.tech>
2025-04-03 23:48:09 +0300

    Initial commit

---
+++ fixtures/pages.py
@@ -0,0 +1,40 @@
+import uuid
+
+import allure
+import pytest
+from playwright.sync_api import Playwright, Page, expect
+
+from config import Settings
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
+
+
+@pytest.fixture
+def chromium_page(playwright: Playwright, settings: Settings) -> Page:
+    expect.set_options(timeout=settings.expect_timeout)
+
+    browser = playwright.chromium.launch(headless=settings.headless)
+    context = browser.new_context(
+        base_url=f"{settings.app_url}/",
+        record_video_dir=settings.videos_dir
+    )
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    page = context.new_page()
+    yield page
+
+    context.tracing.stop(path=settings.tracing_dir.joinpath(f'{uuid.uuid4()}.zip'))
+    browser.close()
+
+    allure.attach.file(settings.tracing_dir.joinpath(f'{uuid.uuid4()}.zip'), name='trace', extension='zip')
+    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
+
+
+@pytest.fixture
+def dashboard_page(chromium_page: Page) -> DashboardPage:
+    return DashboardPage(page=chromium_page)
+
+
+@pytest.fixture
+def registration_page(chromium_page: Page) -> RegistrationPage:
+    return RegistrationPage(page=chromium_page)
